26b61ea69ecb38a39df070f0c52d2720f3b91b05
Samuel <Samueladeyemi2006@gmail.com>
2024-02-02 16:14:14 +0100

    lists

---
+++ Lists/lists.py
@@ -0,0 +1,3 @@
+names = ["Samuel", "Jonh", "Mosh", "Anji"]
+names[1] = "john"
+print(names[0:3])
---
+++ Lists/objects.py
@@ -0,0 +1,4 @@
+numbers = [1,2,3,4,5]
+# numbers.insert(0, -1)
+print(len(numbers))
+print(1 in numbers)
