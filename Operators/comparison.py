3786eca1b68090a901bcdbfad16854aaf77a771c
Samuel <Samueladeyemi2006@gmail.com>
2024-02-02 14:45:47 +0100

    operators

---
+++ Operators/comparison.py
@@ -0,0 +1,9 @@
+x = 3 > 2  # greater than operator
+f = 3 >= 4 # greater or equal to
+s = 3 < 4 # less than operator
+s = 3 <= 4 # less or equal to
+e = 3 == 2 # equality operator
+e = 3 != 2 # inequality operator
+
+print (x)
+print(e)
---
+++ Operators/logical.py
@@ -0,0 +1,8 @@
+price = 40
+
+print(price != 20 and price < 50)
+
+print(price < 20 or price >= 40)
+
+print(not price < 20 )
+
